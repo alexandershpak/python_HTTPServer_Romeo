3016c88f59f23e0b4717364a02cbb21feb6b073c
alexandershpak <alexand.shpak@gmail.com>
2019-03-24 17:21:01 +0300

    space

--- http_server/ex1.py
+++ http_server/ex1.py
@@ -22,6 +22,7 @@ class myHandler(BaseHTTPRequestHandler):
 
     # Handler for the GET requests
     def do_GET(self):
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
